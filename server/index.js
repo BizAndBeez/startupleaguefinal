d0fa354a20f8e39da131408287dd9e25f5a10bbb
vishaldasaritilla <vishaldasaritron@gmail.com>
2025-01-22 12:51:20 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -41,6 +41,8 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+  res.setHeader("Access-Control-Allow-Origin", "https://launch.startupleague.net");
+
   
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
